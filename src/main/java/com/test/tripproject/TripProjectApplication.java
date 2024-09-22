8aa9e1981a2dd405558be2d198088ec4a77716aa
Mr-JackSparrow <132438113+Mr-JackSparrow@users.noreply.github.com>
2024-09-22 16:53:06 +0530

    Project setup is done.

---
+++ src/main/java/com/test/tripproject/TripProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.tripproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/tripproject/TripProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.tripproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
